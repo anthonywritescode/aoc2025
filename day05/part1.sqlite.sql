9ce87cea9db14555c85cbcf5035a54385275e692
anthony sottile <asottile@umich.edu>
2025-12-07 17:31:00 -0500

    improve parsing for day 5 sql

--- day05/part1.sqlite.sql
+++ day05/part1.sqlite.sql
@@ -1,38 +1,27 @@
 CREATE TABLE input (s VARCHAR);
 INSERT INTO input VALUES (TRIM(readfile('input.txt'), char(10)));
 
+CREATE TABLE input2 (ranges VARCHAR, nums VARCHAR);
+INSERT INTO input2
+SELECT
+    SUBSTR(s, 0, INSTR(s, char(10) || char(10))),
+    SUBSTR(s, INSTR(s, char(10) || char(10)) + 2)
+FROM input;
+
 CREATE TABLE ranges (s INT, e INT);
 INSERT INTO ranges
 SELECT value->>'[0]', value->>'[1]'
 FROM json_each((
-    SELECT
-        '[[' ||
-        REPLACE(
-            REPLACE(
-                SUBSTR(s, 0, INSTR(s, char(10) || char(10))),
-                char(10),
-                '],['
-            ),
-            '-',
-            ','
-        ) ||
-        ']]'
-    FROM input
+    SELECT '[[' || REPLACE(REPLACE(ranges, char(10), '],['), '-', ',') || ']]'
+    FROM input2
 ));
 
 CREATE TABLE nums (n INT);
 INSERT INTO nums
 SELECT value
 FROM json_each((
-    SELECT
-        '[' ||
-        REPLACE(
-            SUBSTR(s, INSTR(s, char(10) || char(10)) + 2),
-            char(10),
-            ','
-        ) ||
-        ']'
-    FROM input
+    SELECT '[' || REPLACE(nums, char(10), ',') || ']'
+    FROM input2
 ));
 
 SELECT COUNT(DISTINCT nums.n)
--- day05/part2.sqlite.sql
+++ day05/part2.sqlite.sql
@@ -1,23 +1,19 @@
 CREATE TABLE input (s VARCHAR);
 INSERT INTO input VALUES (TRIM(readfile('input.txt'), char(10)));
 
+CREATE TABLE input2 (ranges VARCHAR, nums VARCHAR);
+INSERT INTO input2
+SELECT
+    SUBSTR(s, 0, INSTR(s, char(10) || char(10))),
+    SUBSTR(s, INSTR(s, char(10) || char(10)) + 2)
+FROM input;
+
 CREATE TABLE ranges (s INT, e INT);
 INSERT INTO ranges
 SELECT value->>'[0]', value->>'[1]'
 FROM json_each((
-    SELECT
-        '[[' ||
-        REPLACE(
-            REPLACE(
-                SUBSTR(s, 0, INSTR(s, char(10) || char(10))),
-                char(10),
-                '],['
-            ),
-            '-',
-            ','
-        ) ||
-        ']]'
-    FROM input
+    SELECT '[[' || REPLACE(REPLACE(ranges, char(10), '],['), '-', ',') || ']]'
+    FROM input2
 ))
 ORDER BY value->>'[0]';
 
