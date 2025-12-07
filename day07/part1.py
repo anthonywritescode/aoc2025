52ea509df4b839bf164a59c03d4458c2559bdedc
anthony sottile <asottile@umich.edu>
2025-12-07 14:28:51 -0500

    day 7

---
+++ day07/part1.py
@@ -0,0 +1,74 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def compute(s: str) -> int:
+    lines = s.splitlines()
+
+    splits = 0
+    positions = {lines[0].index('S')}
+    for line in lines:
+        new_positions: set[int] = set()
+        for position in positions:
+            if line[position] == '^':
+                splits += 1
+                new_positions.update((position - 1, position + 1))
+            else:
+                new_positions.add(position)
+        positions = new_positions
+
+    return splits
+
+
+INPUT_S = '''\
+.......S.......
+...............
+.......^.......
+...............
+......^.^......
+...............
+.....^.^.^.....
+...............
+....^.^...^....
+...............
+...^.^...^.^...
+...............
+..^...^.....^..
+...............
+.^.^.^.^.^...^.
+...............
+'''
+EXPECTED = 21
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
---
+++ day07/part2.py
@@ -0,0 +1,74 @@
+from __future__ import annotations
+
+import argparse
+import collections
+import os.path
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def compute(s: str) -> int:
+    lines = s.splitlines()
+
+    positions = collections.Counter([lines[0].index('S')])
+    for line in lines:
+        new_positions: collections.Counter[int] = collections.Counter()
+        for position, val in positions.items():
+            if line[position] == '^':
+                new_positions[position - 1] += val
+                new_positions[position + 1] += val
+            else:
+                new_positions[position] += val
+        positions = new_positions
+
+    return sum(positions.values())
+
+
+INPUT_S = '''\
+.......S.......
+...............
+.......^.......
+...............
+......^.^......
+...............
+.....^.^.^.....
+...............
+....^.^...^....
+...............
+...^.^...^.^...
+...............
+..^...^.....^..
+...............
+.^.^.^.^.^...^.
+...............
+'''
+EXPECTED = 40
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
