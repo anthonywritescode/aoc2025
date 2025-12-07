bc09bcc03f321d41d8ed9232caba2011eef78497
anthony sottile <asottile@umich.edu>
2025-12-07 17:19:51 -0500

    improve day 3 sql

--- day03/solution.sqlite.sql
+++ day03/solution.sqlite.sql
@@ -2,38 +2,36 @@ CREATE TABLE input (n INT, s VARCHAR);
 -- adjust `n` for part 1 (2) or part 2 (12)
 INSERT INTO input VALUES (12, TRIM(readfile('input.txt'), char(10)));
 
+CREATE TABLE lines (s VARCHAR);
+INSERT INTO lines
+SELECT value FROM json_each((
+    SELECT '["' || REPLACE(s, char(10), '","') || '"]' FROM input
+));
+
 WITH RECURSIVE
-    nn (chars, i, j, left, line, rest, n, dbg)
+    nn (chars, i, j, left, line, n)
 AS (
-    SELECT
-        '', 0, 0, 0,
-        (SELECT SUBSTR(s, 0, INSTR(s, char(10))) FROM INPUT),
-        (SELECT SUBSTR(s || char(10), INSTR(s, char(10)) + 1) FROM input),
-        (SELECT n FROM input), NULL
+    SELECT '', 0, 0, 0, s, (SELECT n FROM input) FROM lines
     UNION ALL
     SELECT
         -- chars
         CASE
-            WHEN nn.i >= nn.n THEN ''
             WHEN nn.j >= LENGTH(nn.line) - (nn.n - nn.i) + 1 THEN
                 nn.chars || SUBSTR(nn.line, nn.left + 1, 1)
             ELSE nn.chars
         END,
         -- i
         CASE
-            WHEN nn.i >= nn.n THEN 0
             WHEN nn.j >= LENGTH(nn.line) - (nn.n - nn.i) + 1 THEN nn.i + 1
             ELSE nn.i
         END,
         -- j
         CASE
-            WHEN nn.i >= nn.n THEN 0
             WHEN nn.j >= LENGTH(nn.line) - (nn.n - nn.i) + 1 THEN 0
             ELSE nn.j + 1
         END,
         -- left
         CASE
-            WHEN nn.i >= nn.n THEN 0
             WHEN nn.j >= LENGTH(nn.line) - (nn.n - nn.i) + 1 THEN 0
             WHEN (
                 SUBSTR(nn.line, nn.j + 1, 1) >
@@ -44,20 +42,13 @@ AS (
         END,
         -- line
         CASE
-            WHEN nn.i >= nn.n THEN
-                SUBSTR(nn.rest, 0, INSTR(nn.rest, char(10)))
             WHEN nn.j >= LENGTH(nn.line) - (nn.n - nn.i) + 1 THEN
                 SUBSTR(nn.line, nn.left + 2)
             ELSE nn.line
         END,
-        -- rest and n
-        CASE
-            WHEN nn.i >= nn.n THEN
-                SUBSTR(nn.rest, INSTR(nn.rest, char(10)) + 1)
-            ELSE nn.rest
-        END,
-        nn.n, SUBSTR(nn.line, nn.j + 1, 1)
+        -- n
+        nn.n
     FROM nn
-    WHERE nn.i < nn.n OR nn.rest != ''
+    WHERE nn.i < nn.n
 )
-SELECT SUM(chars) FROM nn WHERE nn.i >= nn.n
+SELECT SUM(chars) FROM nn WHERE nn.i = nn.n
