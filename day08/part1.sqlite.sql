181b64811ba7fd3729c157f153a2a7bbac0c1aaa
anthony sottile <asottile@umich.edu>
2025-12-08 15:49:33 -0500

    day 8 in sqlite!

---
+++ day08/part1.sqlite.sql
@@ -0,0 +1,81 @@
+CREATE TABLE input (n INT, s VARCHAR);
+INSERT INTO input VALUES (1000, TRIM(readfile('input.txt'), char(10)));
+
+CREATE TABLE points (x INT, y INT, z INT);
+INSERT INTO points
+SELECT value->>'[0]', value->>'[1]', value->>'[2]'
+FROM json_each((
+    SELECT '[[' || REPLACE(s, char(10), '],[') || ']]'
+    FROM input
+));
+
+CREATE TABLE distances (d INT, p1 INT, p2 INT);
+INSERT INTO distances
+SELECT d, p1, p2 FROM (
+    SELECT
+        (
+            pow((p1.x - p2.x), 2) +
+            pow((p1.y - p2.y), 2) +
+            pow((p1.z - p2.z), 2)
+        ) AS d,
+        p1.ROWID AS p1,
+        p2.ROWID AS p2
+    FROM points p1
+    INNER JOIN points p2 ON p2.ROWID > p1.ROWID
+)
+ORDER BY d;
+
+CREATE TABLE largest(n);
+WITH RECURSIVE nn (i, arrs, p1, p2) AS (
+    SELECT
+        ROWID,
+        (SELECT json_group_array(json_array(ROWID)) FROM points),
+        p1,
+        p2
+    FROM distances WHERE ROWID = 1
+    UNION ALL
+    SELECT
+        nn.i + 1,
+        (
+            SELECT json_group_array(json(arr)) FROM (
+                -- all the arrays not containing the two points
+                SELECT value AS arr
+                FROM json_each(nn.arrs) AS o
+                WHERE NOT EXISTS(
+                    SELECT 1 FROM json_each(o.value) AS i
+                    WHERE i.value == nn.p1 OR i.value == nn.p2
+                )
+                UNION ALL
+                -- a single array joining the arrays containing the two points
+                SELECT json_group_array(value) FROM (
+                    SELECT value FROM json_each((
+                        SELECT value FROM json_each(nn.arrs) AS o
+                        WHERE EXISTS (
+                            SELECT 1 FROM json_each(o.value) as i
+                            WHERE i.value == nn.p1
+                        )
+                    ))
+                    UNION
+                    SELECT value FROM json_each((
+                        SELECT value FROM json_each(nn.arrs) AS o
+                        WHERE EXISTS (
+                            SELECT 1 FROM json_each(o.value) as i
+                            WHERE i.value == nn.p2
+                        )
+                   ))
+                )
+            )
+        ),
+        (SELECT p1 FROM distances WHERE ROWID = nn.i + 1),
+        (SELECT p2 FROM distances WHERE ROWID = nn.i + 1)
+    FROM nn
+    WHERE nn.i <= (SELECT n FROM input)
+)
+INSERT INTO largest
+SELECT json_array_length(value) FROM nn, json_each(nn.arrs)
+WHERE nn.i = (SELECT n + 1 FROM input)
+ORDER BY json_array_length(value) DESC LIMIT 3;
+
+SELECT l1.n * l2.n * l3.n
+FROM largest AS l1, largest AS l2, largest AS l3
+WHERE l1.ROWID = 1 AND l2.ROWID = 2 AND l3.ROWID = 3;
